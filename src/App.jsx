3416969ed7fc47b96a86c78ed6ec54397cea7bbc
Satish Pokala <satishpokala124@gmail.com>
2023-05-12 16:16:03 +0530

    Adding project files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <div className="h-screen w-full flex flex-col items-center justify-center gap-4">
+      <p className="text-lg">Hello there!! This is Satish👋</p>
+      <h1 className="font-bold text-3xl">This is my Portfolio</h1>
+    </div>
+  )
+}
+
+export default App
