a13c1897fc04b7fe1c6c2eb35e93c459fcd1f88e
Satish Pokala <satishpokala124@gmail.com>
2023-05-14 01:23:33 +0530

    Added twinkling stars

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Header from 'src/Header';
+import Stars from 'src/Stars';
 
 function App() {
   return (
-    <div className='flex items-center justify-center w-full h-screen gap-4 bg-gradient-to-b from-slate-950 to-sky-900 text-slate-100'>
+    <div className='relative flex items-center justify-center w-full h-screen gap-4 bg-gradient-to-b from-slate-950 to-sky-900 text-slate-100'>
+      <Stars />
       <Header />
     </div>
   );
