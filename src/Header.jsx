8bedfcb9d836851e651d2abb98c129c7db2de912
Satish Pokala <satishpokala124@gmail.com>
2023-05-13 14:55:29 +0530

    Added animation

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import HeaderCSS from './Header.module.css';
 
 export default function Header() {
   return (
-    <div className='flex flex-col'>
-      <p className='text-2xl transition cursor-default hover:translate-x-2'>{`Hello there!! I'm`}</p>
-      <h1 className='mt-2 text-5xl'>Satish Pokala</h1>
-      {/* <h1 className='text-3xl font-bold'>This is my Portfolio</h1> */}
+    <div className={`flex flex-col ${HeaderCSS.parent}`}>
+      <p className={`text-2xl cursor-default ${HeaderCSS.jump}`}>{`Hello there!! I'm`}</p>
+      <h1 className={`mt-2 text-5xl ${HeaderCSS.jump}`}>Satish Pokala</h1>
     </div>
   );
 }
