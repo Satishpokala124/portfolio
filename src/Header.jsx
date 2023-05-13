c060ed36c56ab9d372907f97f5038abd9a7236eb
Satish Pokala <satishpokala124@gmail.com>
2023-05-13 15:00:55 +0530

    Updated style

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
 import HeaderCSS from './Header.module.css';
 
 export default function Header() {
   return (
     <div className={`flex flex-col ${HeaderCSS.parent}`}>
-      <p className={`text-2xl cursor-default ${HeaderCSS.jump}`}>{`Hello there!! I'm`}</p>
-      <h1 className={`mt-2 text-5xl ${HeaderCSS.jump}`}>Satish Pokala</h1>
+      <p className={`text-2xl font-medium cursor-default ${HeaderCSS.jump}`}>
+        {`Hello there!! I'm`}
+      </p>
+      <h1 className={`mt-2 font-bold text-8xl ${HeaderCSS.jump}`}>Satish Pokala</h1>
     </div>
   );
 }
