e41b3c0ec4e0f1683ff8abffb13d226abd6cd810
Satish Pokala <satishpokala124@gmail.com>
2023-05-15 01:12:38 +0530

    Added image mix-blend-mode

--- src/Header.jsx
+++ src/Header.jsx
@@ -9,12 +9,12 @@ export default function Header() {
       <div className='w-full h-full'>
         <Stars />
       </div>
-      <div className='absolute flex flex-col items-center top-1/3'>
+      <div className='absolute flex flex-col items-center top-1/3 md:top-1/3'>
         <p className={`text-2xl cursor-default w-full ${HeaderCSS.jump}`}>{`Hello there!! I'm`}</p>
         <h1 className={`mt-2 font-bold text-6xl text-cyan-400 ${HeaderCSS.jump}`}>Satish Pokala</h1>
       </div>
       <img
-        className='absolute z-10 object-cover object-bottom w-full h-full'
+        className='absolute z-10 object-cover object-bottom w-full h-full mix-blend-luminosity'
         src='images/bg-mountains.png'
         alt='bg-mountains'
       />
