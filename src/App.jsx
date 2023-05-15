9e77d85641596b45fd1ef888ca99c2dc616cd870
Satish Pokala <satishpokala124@gmail.com>
2023-05-15 22:22:43 +0530

    changed bg-mountains image

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import Header from 'src/Header';
 
 function App() {
   return (
-    <div className='flex flex-col items-center justify-center w-full overflow-y-auto'>
+    <div className='w-full h-screen overflow-x-hidden overflow-y-auto preserve-3d'>
       <Header />
-      <div className='flex items-center justify-center w-full h-screen text-6xl font-extrabold text-white bg-[#001522]'>
+      <div className='flex items-center justify-center w-full h-screen text-6xl font-extrabold text-white bg-[#0c0904]'>
         <p className='bg-pan'>{`I'm a Fullstack developer`}</p>
       </div>
     </div>
--- src/Header.jsx
+++ src/Header.jsx
@@ -3,21 +3,23 @@ import Stars from './Stars';
 
 export default function Header() {
   return (
-    <div
-      className={`relative flex flex-col items-center justify-center h-screen w-full bg-gradient-to-b from-slate-950 to-sky-950 text-slate-100 p-0 m-0 ${HeaderCSS.parent}`}
-    >
-      <div className='w-full h-full'>
+    <div className='relative flex items-center justify-center w-full h-screen -z-10 perspective-10'>
+      <div
+        className={`absolute w-full h-full bg-gradient-to-b from-slate-950 to-sky-950 ${HeaderCSS.bgStars}`}
+      >
         <Stars />
       </div>
-      <div className='absolute flex flex-col items-center top-1/3 md:top-1/3'>
-        <p className={`text-2xl cursor-default w-full ${HeaderCSS.jump}`}>{`Hello there!! I'm`}</p>
-        <h1 className={`mt-2 font-bold text-6xl text-cyan-400 ${HeaderCSS.jump}`}>
+      <div className='absolute flex flex-col items-center top-1/3 md:top-1/4'>
+        <p className={`text-2xl cursor-default w-full text-white ${HeaderCSS.jump}`}>
+          {`Hello there!! I'm`}
+        </p>
+        <p className={`mt-2 font-bold text-6xl ${HeaderCSS.jump}`}>
           <span className='bg-pan'>Satish Pokala</span>
-        </h1>
+        </p>
       </div>
       <img
-        className='absolute z-10 object-cover object-bottom w-full h-full mix-blend-luminosity'
-        src='images/bg-mountains.png'
+        className={`absolute object-cover w-full h-full ${HeaderCSS.bgMountains}`}
+        src='images/bg-mountains-2.png'
         alt='bg-mountains'
       />
     </div>
