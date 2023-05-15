f0561b047ce3e8d2f9a33bbef7dfb9da7764ce7b
Satish Pokala <satishpokala124@gmail.com>
2023-05-15 15:27:32 +0530

    Added animations

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import Header from 'src/Header';
 
 function App() {
   return (
-    <div className='flex flex-col items-center justify-center w-full'>
+    <div className='flex flex-col items-center justify-center w-full overflow-y-auto'>
       <Header />
+      <div className='flex items-center justify-center w-full h-screen text-6xl font-extrabold text-white bg-[#001522]'>
+        <p className='bg-pan'>{`I'm a Fullstack developer`}</p>
+      </div>
     </div>
   );
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -4,14 +4,16 @@ import Stars from './Stars';
 export default function Header() {
   return (
     <div
-      className={`relative flex flex-col items-center justify-center h-screen w-full bg-gradient-to-b from-slate-950 to-sky-950 text-slate-100 ${HeaderCSS.parent}`}
+      className={`relative flex flex-col items-center justify-center h-screen w-full bg-gradient-to-b from-slate-950 to-sky-950 text-slate-100 p-0 m-0 ${HeaderCSS.parent}`}
     >
       <div className='w-full h-full'>
         <Stars />
       </div>
       <div className='absolute flex flex-col items-center top-1/3 md:top-1/3'>
         <p className={`text-2xl cursor-default w-full ${HeaderCSS.jump}`}>{`Hello there!! I'm`}</p>
-        <h1 className={`mt-2 font-bold text-6xl text-cyan-400 ${HeaderCSS.jump}`}>Satish Pokala</h1>
+        <h1 className={`mt-2 font-bold text-6xl text-cyan-400 ${HeaderCSS.jump}`}>
+          <span className='bg-pan'>Satish Pokala</span>
+        </h1>
       </div>
       <img
         className='absolute z-10 object-cover object-bottom w-full h-full mix-blend-luminosity'
